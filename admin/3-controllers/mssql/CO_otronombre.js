3773e02eaa5ce75439c3826d49e36bb24fec274b
Angel Rodriguez <angel.rodriguez810@gmail.com>
2019-04-08 10:37:19 -0400

    Optimización 60%

---
+++ admin/3-controllers/mssql/CO_otronombre.js
@@ -0,0 +1,68 @@
+app.controller("ms_all", function ($scope, $http, $compile) {
+    ms_all = this;
+    ms_all.fixFilters = [
+        {
+            field: 'id',
+            operator: ">",
+            value: 1000
+        }
+    ];
+    RUNCONTROLLER("ms_all", ms_all, $scope, $http, $compile);
+    ms_all.formulary = function (data, mode, defaultData) {
+        if (ms_all !== undefined) {
+            RUN_B("ms_all", ms_all, $scope, $http, $compile);
+
+            ms_all.form.modalWidth = ENUM.modal.width.full;
+            ms_all.form.titles = {
+                new: "Nuevo ALL",
+                edit: "`Editar ALL - ${$scope.name}`",
+                view: "`Ver ALL - ${$scope.name}`"
+            };
+            ms_all.form.readonly = {};
+            ms_all.createForm(data, mode, defaultData);
+            ms_all.$scope.$watch('ms_all.name', function (value) {
+                var rules = [];
+                rules.push(VALIDATION.general.required(value));
+                rules.push(VALIDATION.text.realdata(value));
+                VALIDATION.validate(ms_all, "name", rules);
+            });
+            ms_all.$scope.$watch('ms_all.child', function (value) {
+                var rules = [];
+                rules.push(VALIDATION.general.required(value));
+                VALIDATION.validate(ms_all, "child", rules);
+            });
+            ms_all.$scope.$watch('ms_all.products', function (value) {
+                var rules = [];
+                rules.push(VALIDATION.general.required(value));
+                rules.push(VALIDATION.dropdown.atLeast(value, 3));
+                rules.push(VALIDATION.dropdown.atLeast(value, 2, true));
+                VALIDATION.validate(ms_all, "products", rules);
+            });
+            ms_all.$scope.$watch('ms_all.salary', function (value) {
+                var rules = [];
+                rules.push(VALIDATION.general.required(DSON.cleanNumber((value || ""))));
+                VALIDATION.validate(ms_all, "salary", rules);
+            });
+            ms_all.$scope.$watch('ms_all.average', function (value) {
+                var rules = [];
+                var valueClean = DSON.cleanNumber((value || ""));
+                ms_all.salaryNeto = (DSON.cleanNumber(ms_all.salary) * DSON.cleanNumber(value)) / 100;
+                if (typeof ms_all.form.masked === 'function')
+                    ms_all.salaryNeto = ms_all.form.masked("salaryNeto", Number(ms_all.salaryNeto).toFixed(2));
+                rules.push(VALIDATION.general.required(valueClean));
+                rules.push(VALIDATION.number.range(valueClean, 1, 100));
+                VALIDATION.validate(ms_all, "average", rules);
+            });
+            ms_all.$scope.$watch('ms_all.image_DragonCountFile', function (value) {
+                var rules = [];
+                rules.push(VALIDATION.file.count(value, 1));
+                VALIDATION.validate(ms_all, "image", rules);
+            });
+            ms_all.$scope.$watch('ms_all.color', function (value) {
+                var rules = [];
+                rules.push(VALIDATION.text.noContainsColor(value, ['rgb(255, 0, 0)']));
+                VALIDATION.validate(ms_all, "color", rules);
+            });
+        }
+    };
+});
---
+++ admin/4-crud/mssql/otronombre/1columns.js
@@ -0,0 +1,92 @@
+CRUD_ms_product = {};
+DSON.keepmerge(CRUD_ms_product, CRUDDEFAULTS);
+DSON.keepmerge(CRUD_ms_product, {
+    table: {
+        engine: 'ms',
+        columns: {
+            id: {
+                label: "ID",
+                sorttype: "numeric",
+                class: "text-left",
+                exportExample: false
+            },
+            name: {
+                label: "Nombraso",
+                shorttext: 20
+            },
+            price: {
+                label: "price",
+                formattype: "numeric:0,0.00",
+                sorttype: "numeric",
+                class: "text-right"
+            },
+            ms_category_name: {
+                label: "Category",
+                shorttext: 20,
+                link: {
+                    table: "ms_category",
+                    from: "category",
+                    modal: {
+                        header: {
+                            title: MESSAGE.i(`columns.ms_category_name`,'Category'),
+                            icon: ICON.classes.archive
+                        },
+                        footer: {
+                            cancelButton: true
+                        },
+                        content: {
+                            loadingContentText: MESSAGE.i('actions.Loading'),
+                        },
+                    }
+                }
+            },
+            description: {
+                label: "description",
+                sortable: false,
+                shorttext: 20,
+                null: "<span class='text-grey'>[NULL]</span>"
+            },
+            active: {
+                visible: true,
+                sorttype: "bool",
+                formattype: "bool"
+            },
+            created: {
+                visible: false,
+                sorttype: "time",
+                formattype: "datetime>DD-MM-YYYY hh:mm a",
+                exportExample: false
+            },
+            updated: {
+                visible: false,
+                visibleDetail: false,
+                export: false,
+                exportExample: false
+            },
+            deleted: {
+                visible: false,
+                visibleDetail: false,
+                export: false,
+                exportExample: false
+            },
+            user_created: {
+                visible: false,
+                visibleDetail: false,
+                export: false,
+                exportExample: false
+            },
+            user_updated: {
+                visible: false,
+                visibleDetail: false,
+                export: false,
+                exportExample: false
+            },
+            user_deleted: {
+                visible: false,
+                visibleDetail: false,
+                export: false,
+                exportExample: false
+            }
+        },
+    }
+});
